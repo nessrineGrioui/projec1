1bbdabe2216758d3349518b306699039ea150f39
nessrineGrioui <48864593+nessrineGrioui@users.noreply.github.com>
2020-01-14 13:04:37 +0100

    Add files via upload

---
+++ project1.java
@@ -0,0 +1,10 @@
+classe public HelloWorld {   
+
+    public static void main ( String [] args ) { 
+        // Imprime "Hello, World" dans la fenêtre du terminal.        
+         Système . dehors . println ( "Bonjour, monde" ); }    
+        
+
+    
+
+}
